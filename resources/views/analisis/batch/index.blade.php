cd89939f022739210dcb179cf66d660e668c7b64
Rahmat Hadinata <rsafei731@gmail.com>
2026-06-30 11:54:10 +0700

    Fix delete icon SVG width and height classes

--- resources/views/analisis/batch/index.blade.php
+++ resources/views/analisis/batch/index.blade.php
@@ -62,7 +62,7 @@
                         <button type="button"
                                 @click="openConfirmModal = true; confirmTitle = 'Hapus Batch Penelitian'; confirmMessage = 'Apakah Anda yakin ingin menghapus batch &quot;{{ addslashes($batch->name) }}&quot;? Seluruh data responden di dalamnya akan terhapus secara permanen.'; confirmAction = '{{ route('analisis.batch.destroy', $batch->id) }}'"
                                 class="p-1.5 text-outline hover:text-error hover:bg-error-container rounded-lg transition-colors" title="Hapus Batch">
-                            <svg class="w-4.5 h-4.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/></svg>
+                            <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/></svg>
                         </button>
                     </div>
                 </div>
